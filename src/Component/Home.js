ac9d3b29beb1360f74e88e819dd5ae7d0bd60c48
SAN13MATHI <145042972+SAN13MATHI@users.noreply.github.com>
2023-11-10 00:17:57 +0530

    Update Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -5,7 +5,7 @@ function Home() {
         <section className="home">
             <h1>SANMATHI PRIYA K R</h1>
             <h1>My Portfolio</h1>
-            <p>Full stack developer| Codeer</p>
+            <p>Full stack developer| Coder</p>
         </section>
     );
 }
