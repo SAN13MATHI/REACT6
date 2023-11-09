e516009a5618a29e46d3c97eedc43dd5ea045d82
SAN13MATHI <145042972+SAN13MATHI@users.noreply.github.com>
2023-11-10 00:13:11 +0530

    Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Home() {
     return (
         <section className="home">
-            <h1>P VISHNU MOHAN KRISHNA</h1>
-            <h1>Welcome to My Portfolio</h1>
-            <p>Front-End Developer | React Enthusiast</p>
+            <h1>SANMATHI PRIYA K R</h1>
+            <h1>My Portfolio</h1>
+            <p>Full stack developer| Codeer</p>
         </section>
     );
 }
